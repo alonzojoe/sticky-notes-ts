85e338f607ea63d0edf3e98dd3a6f6a10f440e9c
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-09-06 16:53:17 +0800

    feat: add useState with ts and a function that add a note

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
+import React, { useState } from "react";
 import StickyNote from "./components/StickyNote";
 import Header from "./components/Header";
 import StickyNoteImg from "./assets/sticky.png";
 
+type Note = {
+  id: number;
+  title: string;
+  description: string;
+};
+
 function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  const handleFormSubmit = () => {
+    setNotes((prevNotes) => {
+      const newNote = {
+        id: Date.now(),
+        title: "New Note",
+        description: "This is a new note",
+      };
+      return [newNote, ...prevNotes];
+    });
+  };
+
   return (
     <main>
       <Header image={{ src: StickyNoteImg, alt: "A list of notes" }}>
         <h1>Your Sticky Notes</h1>
       </Header>
+      <button onClick={handleFormSubmit}>Add Note</button>
+      <pre>{JSON.stringify(notes)}</pre>
       <StickyNote
         title="My Note"
         description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum dolor sit est possimus eaque iusto, veritatis consectetur nam "
